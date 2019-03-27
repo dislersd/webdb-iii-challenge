3056c12db868d625d3a7e388e8d321cf56c0796d
dislersd <dislersd@gmail.com>
2019-03-27 13:20:05 -0700

    cohorts migration complete

--- migrations/20190327130756_cohorts.js
+++ migrations/20190327130756_cohorts.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("cohorts", (tbl) => {
+    tbl.increments();// primary key
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("cohorts")
 };
