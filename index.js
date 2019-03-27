4861f6a4f8f2076c6d35e2c2ee1b2638c9774694
dislersd <dislersd@gmail.com>
2019-03-27 12:57:49 -0700

    created server and index

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./server.js')
 
 const port = 3000;
 server.listen(port, function() {
-  console.log(`\n Server running on port ${port}`)
+  console.log(`\n ** Server running on port ${port} **`)
 })
